663b189f6929dd2ac7964002a9afe3dc5222cebc
Yuri <yuribzfreitas@gmail.com>
2020-11-19 19:01:07 -0300

    Server jwt

---
+++ server/src/index.ts
@@ -0,0 +1,11 @@
+import 'reflect-metadata';
+import './database/connect';
+import express from 'express';
+import router from './router'
+
+
+const app = express();
+
+app.use(express.json());
+app.use(router);
+app.listen(3000,() => console.log(''));
